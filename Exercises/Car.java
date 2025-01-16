f8b02583af1a415b9ee407388932bed2efaaf1e9
Soma-XI <W0453163@campus.nscc.ca>
2025-01-15 21:44:54 -0400

    Add files via upload

---
+++ Exercises/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+String name;
+int model;
+String color;
+
+
+public String getName () {
+return name;
+
+}
+
+public void setName(String m) {
+name = m;    
+
+   }
+
+}
---
+++ Exercises/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+ 
+public static void main(String[] args) {
+
+
+
+Car C1 = new Car ();
+  C1.name = "Dodge";
+  C1.color = "red";
+  C1.model = 2025;
+
+  
+
+
+  Car C2 = new Car ();
+  C2.name = "Yukon";
+  C2.color = "white";
+  C2.model = 2019;
+
+  
+  System.out.println(C2.getName());
+  
+C2.setName(m:"Example");
+
+  System.out.println(C2.getName());
+ 
+}
+
+}
